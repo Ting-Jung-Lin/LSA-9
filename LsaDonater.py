83a5b62231a33c38e3b34f47c7bf528835e327fb
Sarah <sarah.yenting@gmail.com>
2022-01-08 15:15:35 +0800

    update LsaDonater.py

--- LsaDonater.py
+++ LsaDonater.py
@@ -1,7 +1,7 @@
 import telepot
 import time
 from telepot.loop import MessageLoop
-from pprint import pprint
+from picamera import PiCamera
 
 # LsaDonate Token
 bot = telepot.Bot('5015633065:AAEJNx8OoHl2fjBDUR1DXDIFMoxdD2rYuN8')
@@ -13,6 +13,9 @@ def handle(msg):
     # 開始連線，向 bot 訂閱通知
     if telegramText == '/start':
         bot.sendMessage(chat_id, 'Hi! This is a box you can donate some money:)')
+
+#當偵測到有人投錢，就回傳照片與時間
+
 
 MessageLoop(bot, handle).run_as_thread()
 print("I'm listening...")
